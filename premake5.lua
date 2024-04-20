57fa1584b9533e5ffa48a7ea05e91bc0979af6cf
Mich-Dich <mally.dominik@gmx.de>
2024-04-20 23:02:45 +0200

    update

---
+++ premake5.lua
@@ -0,0 +1,52 @@
+project "fastgltf"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputs .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputs .. "/%{prj.name}")
+
+	files
+	{
+		"include/fastgltf/base64.hpp",
+		"include/fastgltf/core.hpp",
+		"include/fastgltf/glm_element_traits.hpp",
+		"include/fastgltf/tools.hpp",
+		"include/fastgltf/types.hpp",
+		"include/fastgltf/util.hpp",
+		"src/fastgltf.cpp",
+		"src/base64.cpp",
+
+		"deps/simdjson/simdjson.h",
+		"deps/simdjson/simdjson.cpp",
+	}
+
+	includedirs
+	{
+		"deps/simdjson",
+		"include",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:RelWithDebInfo"
+		-- buildoptions "/MD"
+		-- defines "PFF_RELEASE_WITH_DEBUG_INFO"
+		runtime "Release"
+		symbols "on"
+		optimize "speed"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
